78af472135f9caf0b02f3030eded43b0c698d865
Lucky Tamrakar <67165145+LuckyTamrakar@users.noreply.github.com>
2022-05-26 12:14:36 +0530

    Add files via upload

---
+++ otp-authentication-with-django-main/otpAuthentication/urls.py
@@ -0,0 +1,31 @@
+"""otpAuthentication URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.http import HttpResponseRedirect
+
+from authApp.forms import John, Riya, Larry, James, Himanshi, Sohil
+from authApp.views import appointment1, appointment2, appointment3, appointment4, appointment5, appointment6
+
+
+urlpatterns = [
+    path('user/Lucky-admin/Wecare', admin.site.urls),
+    path('',include('authApp.urls')), 
+    
+    path(r'captcha/', include('captcha.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
